7f244ef94a44e38dde9509797fc9cfec34dd32aa
CatarinaJecev <lolpoplol5720@gmail.com>
2024-02-29 20:06:01 +0200

    first commit

---
+++ src/lesson1/DataTypes.java
@@ -0,0 +1,21 @@
+package lesson1;
+
+public class DataTypes {
+    public static void main(String[] args){
+      System.out.println("INTEGER DATA TYPES");
+
+    byte b = 127;
+    short s = 32;
+    int i = 1;
+    long l = 356;
+
+    System.out.prinln("Byte type: from -128 to 127. " +
+            "Example: b = " + b);
+    System.out.prinln("Short type: from -32,768 to 32,767. " +
+        "Example: s = " + s);
+    System.out.prinln("Int type: from -2,174,483,648 to 2,147,483,647. " +
+            "Example: i = " + i);
+    System.out.prinln("Long type: from 9,223,372,036,854,775,808 to 9,223,372,036,854,775,807. " +
+            "Example: l = " + l);
+    }
+}
---
+++ src/start.java
@@ -0,0 +1,11 @@
+public class start {
+    public static void main(String[] args){
+        int myFirstVariable;
+
+        myFirstVariable = 1;
+
+        System.out.println(myFirstVariable);
+
+
+    }
+}
